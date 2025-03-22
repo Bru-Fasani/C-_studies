3b75213671d3f600f8bbbe9d3ad530b821789ea8
Bru-Fasani <brunafasani.arts@gmail.com>
2025-03-22 16:26:16 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-string projectName = "ACME";
+﻿string[] orderIDs = {"B123", "C234", "A345","C15","B177","G3003","C235","B179"};
 
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-Console.WriteLine($"View English output:\n\tC:\\Exercise\\{projectName}\\data.txt\n");
-Console.WriteLine($"{russianMessage}:\n\tC:\\Exercise\\{projectName}\\ru-RU\\data.txt");
+foreach(string orderID in orderIDs)
+{
+    if(orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
